e1b16e8c1dce4bf3f84b21296cc5c2ec28c51b88
Baudi <baudirguanche@gmail.com>
2022-05-05 10:42:24 +0100

    Feature: Se ha añadido la opción de introducir números hasta que pulse Q, si no es Q se convierte el String a numero flotante

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String numero = "";
+
+
+        ColasUtils colas = new ColasUtils(4);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (!numero.equals("Q")) {
+
+            System.out.println("Introduce un número o pulsa Q para salir");
+
+            numero = sc.nextLine();
+
+            if (!numero.equals("Q")) {
+                float numeroFlotante = Float.parseFloat(numero);
+                colas.enqueue(numeroFlotante);
+            } else if (numero.equals("Q")) {
+                System.out.println(colas);
+            }
+
+        }
+
+
 
     }
 }
