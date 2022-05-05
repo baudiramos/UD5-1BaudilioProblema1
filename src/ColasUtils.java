310643a25b936e09f7838272ddf51cf805d64a33
Baudi <baudirguanche@gmail.com>
2022-05-05 09:47:10 +0100

    Feature: Se ha añadido los métodos insert y add

--- src/ColasUtils.java
+++ src/ColasUtils.java
@@ -1,7 +1,34 @@
 public class ColasUtils<T> {
-   
 
+    private int numeros;
+    private Object data[];
+    private int numeroValores;
 
+    public void insert(T element, int i) {
 
+        if (numeroValores == data.length) {
+            Object []aux = data;
+            data = new Object[data.length * 2];
+
+            for(int j = 0; j < numeroValores; j++) {
+                data[j] = aux[j];
+            }
+
+            aux = null;
+        }
+
+        for (int j = numeroValores - 1; j >= i; j--) {
+            data[j + 1] = data[j];
+        }
+
+        data[i] = element;
+        numeroValores++;
+    }
+
+    public void add(T element) {
+        insert(element, numeroValores);
+    }
+
+    
 
 }
