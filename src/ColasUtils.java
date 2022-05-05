a3dceb1cf573005bfe6c1bb632a55334bf281613
Baudi <baudirguanche@gmail.com>
2022-05-05 10:41:17 +0100

    Feature: Se ha añadido el método sumarValores y se ha sobreescrito el toString

--- src/ColasUtils.java
+++ src/ColasUtils.java
@@ -1,32 +1,46 @@
+import java.util.Arrays;
+
 public class ColasUtils<T> {
 
     private float numeros;
     private Object data[];
-    private int numeroValores;
+    private int cantidadValores = 0;
 
-    public void insert(T element, int i) {
+    public ColasUtils(int initialCapacity) {
+        data = new Object[initialCapacity];
+    }
 
-        if (numeroValores == data.length) {
+    public Object getElement(int i) {
+        return data[i];
+    }
+
+    public int length() {
+        return cantidadValores;
+    }
+
+    public void insert(Object element, int i) {
+
+        if (cantidadValores == data.length) {
             Object []aux = data;
             data = new Object[data.length * 2];
 
-            for(int j = 0; j < numeroValores; j++) {
+            for(int j = 0; j < cantidadValores; j++) {
                 data[j] = aux[j];
             }
 
             aux = null;
         }
 
-        for (int j = numeroValores - 1; j >= i; j--) {
+        for (int j = cantidadValores - 1; j >= i; j--) {
             data[j + 1] = data[j];
         }
 
         data[i] = element;
-        numeroValores++;
+        cantidadValores++;
     }
 
     public void add(T element) {
-        insert(element, numeroValores);
+        insert(element, cantidadValores);
     }
 
     public void enqueue (T element) {
@@ -35,10 +49,10 @@ public class ColasUtils<T> {
 
     public T delete (int i) {
         Object aux = data[i];
-        for (int j = 0; j < numeroValores; j++) {
+        for (int j = 0; j < cantidadValores; j++) {
             data[j] = data[j + 1];
         }
-        numeroValores--;
+        cantidadValores--;
         return (T) aux;
     }
 
@@ -46,4 +60,19 @@ public class ColasUtils<T> {
         return (T)  delete(0);
     }
 
+    public float sumarValores() {
+        float suma = 0;
+        for (int i = 0; i < cantidadValores; i++) {
+            suma += (float) data[i];
+        }
+        return suma;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output += "\nSuma de valores del Array: " + sumarValores();
+
+        return output;
+    }
 }
